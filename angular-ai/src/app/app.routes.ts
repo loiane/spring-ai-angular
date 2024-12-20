89ca98c8292742352e7d0fa69577277ecf4f9054
Loiane <loianeg@gmail.com>
2024-12-20 16:55:10 -0500

    removed angular v17

--- angular-ai/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'chat', pathMatch: 'full' },
-  { path: 'chat',
-    loadComponent: () => import('./chat/chat.component').then(c => c.ChatComponent)
-  },
-  { path: '**', redirectTo: 'chat' }
-];
