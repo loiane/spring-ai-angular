47a400a46a1cce076f35275a411444f4611e0a85
Loiane <loianeg@gmail.com>
2024-12-20 20:17:07 -0500

    add ChatResponse record and implement chat memory endpoint in OpenAIChatController

--- api-ai/src/main/java/com/loiane/api_ai/chat/OpenAIChatController.java
+++ api-ai/src/main/java/com/loiane/api_ai/chat/OpenAIChatController.java
@@ -1,6 +1,8 @@
 package com.loiane.api_ai.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class OpenAIChatController {
 
     private final ChatClient chatClient;
-
-    record ChatResponse(String message){}
+    private final ChatClient chatClientMemory;
 
     public OpenAIChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
+        this.chatClientMemory = chatClientBuilder
+                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory())).build();
     }
 
     @PostMapping("/chat")
     public ChatResponse chat(@RequestBody String message) {
         var response = this.chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+        return new ChatResponse(response);
+    }
+
+    @PostMapping("/chat-memory")
+    public ChatResponse chatMemory(@RequestBody String message) {
+        var response = this.chatClientMemory.prompt()
                 .user(message)
                 .call()
                 .content();
