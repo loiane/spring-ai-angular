39ac345abbc7ed870255b288eb7af1a68c34c33e
Loiane <loianeg@gmail.com>
2024-12-26 21:40:18 -0500

    Refactor book prompt handling and enhance functionality.
    Updated `BookPromptService` to include system messages with defined tones and support multi-message prompts, improving prompt flexibility. Added `description` field to the `Book` model and updated templates to reflect this change. Removed unused GET request in `api.http` for cleanup.

--- api-ai/src/main/java/com/loiane/api_ai/booksprompt/Book.java
+++ api-ai/src/main/java/com/loiane/api_ai/booksprompt/Book.java
@@ -1,4 +1,4 @@
 package com.loiane.api_ai.booksprompt;
 
-public record Book(String bookName, String isbn) {
+public record Book(String bookName, String isbn, String description) {
 }
--- api-ai/src/main/java/com/loiane/api_ai/booksprompt/BookPromptService.java
+++ api-ai/src/main/java/com/loiane/api_ai/booksprompt/BookPromptService.java
@@ -1,12 +1,15 @@
 package com.loiane.api_ai.booksprompt;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -29,8 +32,14 @@ public class BookPromptService {
     }
 
     public Author listBooksByAuthorFormatted(String author) {
+        String system = "You are a book lover assistant system. For each book description, asnwer with the tone of a Tolkien elf.";
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(system);
+        Message systemMessage = systemPromptTemplate.createMessage();
+
         PromptTemplate promptTemplate = new PromptTemplate("Generate a list of books written by {authorName}?");
-        Prompt prompt = promptTemplate.create(Map.of("authorName", author));
+        Message userMessage = promptTemplate.createMessage(Map.of("authorName", author));
+
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
 
         return chatClient.prompt(prompt).call().entity(Author.class);
     }
