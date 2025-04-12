804ce6ef3aea17ff0fcbbf0d695fece34185ab08
Loiane <loianeg@gmail.com>
2025-04-12 11:33:32 -0400

    Refactor Simple Chat component for improved structure and functionality

--- ui-ai/src/app/simple-chat/simple-chat.component.ts
+++ ui-ai/src/app/simple-chat/simple-chat.component.ts
@@ -1,39 +1,53 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-simple-chat',
-  imports: [MatCardModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, FormsModule, MatToolbar, NgClass, MatIconModule],
   templateUrl: './simple-chat.component.html',
   styleUrl: './simple-chat.component.scss'
 })
 export class SimpleChatComponent {
 
-  message = '';
+  userInput = '';
   isLoading = false;
 
+  message = new FormControl('');
+
   messages = signal([
-    { message: 'Hello, how can I help you today?', isBot: true },
+    { text: 'Hello, how can I help you today?', isBot: true },
   ]);
 
   sendMessage(): void {
-
-    this.message.trim();
-
-    if (!this.message) {
-      return;
+    this.trimUserMessage();
+    if (this.userInput !== '') {
+      this.updateMessages(this.userInput);
+      this.isLoading = true;
+      this.userInput = '';
+      this.getResponse();
     }
-
-    this.updateMessages(this.message);
-    this.isLoading = true;
   }
 
-  private updateMessages(message: string, isBot = false) {
-    this.messages.update(messages => [...messages, { message, isBot }]);
-    this.isLoading = false;
-    this.message = '';
+  private trimUserMessage() {
+    this.userInput = this.userInput.trim();
+  }
+
+  private updateMessages(text: string, isBot = false) {
+    this.messages.update(messages => [...messages, { text, isBot }]);
+  }
+
+  private getResponse() {
+    // TODO: Replace this with actual logic to get response from ChatGPT or a similar service
+    setTimeout(() => {
+      const response = 'This is a simulated response from ChatGPT.';
+      this.updateMessages(response, true);
+      this.isLoading = false;
+    }, 2000);
   }
 }
