56565adedfaea1438c3491fc4b76614d55a9fb30
Loiane <loianeg@gmail.com>
2025-04-27 16:56:42 -0400

    feat: add SimpleLoggerAdvisor to MemoryChatService and enable debug logging for Spring AI

--- api-ai/src/main/java/com/loiane/api_ai/chat/MemoryChatService.java
+++ api-ai/src/main/java/com/loiane/api_ai/chat/MemoryChatService.java
@@ -2,6 +2,7 @@ package com.loiane.api_ai.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,12 @@ public class MemoryChatService {
 
     public MemoryChatService(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder
-                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory()),
+                        new SimpleLoggerAdvisor()
+                )
                 .build();
+
     }
 
     public String chat(String message) {
