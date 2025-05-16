5bd0e791c9bce78ca6bb033faa01b02700bc4d09
Loiane <loianeg@gmail.com>
2025-05-16 18:08:23 -0400

    refactor: swap artifact dependencies in pom.xml and add spring-boot-docker-compose dependency

--- api-ai/src/main/java/com/loiane/api_ai/rag/RagPDFReader.java
+++ api-ai/src/main/java/com/loiane/api_ai/rag/RagPDFReader.java
@@ -14,6 +14,7 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
 import java.io.File;
